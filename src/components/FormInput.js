c36b56ae99bd957361c4dae05faab8efda642c1f
Filip <filip.ingemarsson@live.com>
2018-08-20 10:47:02 +0200

    added disable to input

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './../App.css';
     
+
 function FormInput(props) {    
 
-    return (
-        
+    return (    
         <React.Fragment>
-        
            <div className="form-group">
               <label labelfor={props.name}> {props.label} </label>
-                <input 
-                    type={props.type} 
-                    name={props.name} 
-                    id={props.name} 
-                    placeholder={props.state}
-                    onChange={props.onChange}
-                    value={props.value}
-                    // Get this ternary to work:
-                    //{props.state === '' ? disabled : ''}
-                />
+              <input 
+                type={props.type} 
+                name={props.name} 
+                id={props.name} 
+                placeholder={props.state}
+                onChange={props.onChange}
+                value={props.value}
+                disabled={props.state}
+              />
             </div>
 
         </React.Fragment>
