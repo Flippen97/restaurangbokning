deb54136900d88b326ba84a4b894c5662d0ae407
Ida <ida.bergstrom@medieinstitutet.se>
2018-08-22 10:58:00 +0200

    Counting number of bookings on each day in bookingsArray

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,23 +8,64 @@ class Calendar extends React.Component {
   state = {
   }
 
+  
+
+
+    countBookings = (fetchedArray) => {
+        console.log(fetchedArray); 
+    }
+
+
     
     componentDidMount = () => {   
-        fetch('https://www.idabergstrom.se/restaurant-api/product/read.php')
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(myJson) {
-            let array = myJson.records;
-                console.log(array);
-            for(let i = 0; i < array.length; i++){
-                console.log(array[i].bdate);
-            }
-          });
-    }
-    
-    
+            fetch('https://www.idabergstrom.se/restaurant-api/product/read.php')
+              .then(function(response) {
+                return response.json();
+              })
+              .then(function(myJson) {
+                
+                let data = myJson.records;
+                
+                let calenderContent = [...data];
+                let bookingArray = [];
+                
+                for(let i = 0; i < calenderContent.length; i++){
+                    let bookingObj = { bdate: calenderContent[i].bdate, btime: calenderContent[i].btime }
+                        bookingArray.push(bookingObj);
+                }
+                
+                var counts = {};
+                bookingArray.forEach(function(x) { counts[x.bdate] = (counts[x.bdate] || 0)+1; });
+                console.log(counts);
+                
+                console.log(bookingArray);
+                
+                
+                
+//                let hej = calenderContent.map(bookingObj => {
+//                    let bookingObj = { bdate: calenderContent.bdate, btime: calenderContent.btime }
+//                    bookingObj = { bdate: calenderContent.bdate, btime: calenderContent.btime }
+////                    console.log(bookingObj);
+//                })
+//                
+//                console.log(hej);
+////                let datacopy = [...data[i].bdate];
 
+                
+                
+
+//var counts = {};
+//datacopy.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+//console.log(counts);
+                
+//                for(let i = 0; i < datacopy.length; i++){
+//                    console.log(datacopy[i].bdate);
+//                }
+                
+                
+                
+              }); 
+    }
     
 
     
