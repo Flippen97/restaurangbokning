562110a78e3204d806a5d1b15f44feb5860cf326
Ida <ida.bergstrom@medieinstitutet.se>
2018-08-31 11:24:42 +0200

    Hmm..

--- server/update.php
+++ server/update.php
@@ -7,7 +7,7 @@
 
     $statement = $pdo->prepare(
       "UPDATE customers SET name = :name, email = :email, telephone = :telephone WHERE id = :id;
-        UPDATE bookings SET bdate = :bdate, btime = :btime WHERE customerId = :id"
+        UPDATE bookings SET bdate = :bdate, btime = :btime, numberOfGuests = :numberOfGuests WHERE customerId = :id"
     );
 
     $statement->execute(array(
@@ -17,6 +17,7 @@
       ":telephone" => $array["telephone"],
       ":bdate"     => $array["bdate"],
       ":btime"     => $array["btime"],
+      "numberOfGuests" => $array["numberOfGuests"]
     ));
 
 ?>
