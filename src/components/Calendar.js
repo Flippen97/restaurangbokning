e1caedf2753adfaa62de3ca8417bdb2aeb35aca4
Ida <ida.bergstrom@medieinstitutet.se>
2018-09-03 20:40:02 +0200

    Wrote some comments and did som clean up in Book.js

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -18,14 +18,17 @@ function Calendar(props) {
                 <DayPicker 
                     onDayClick={props.onDayClick}
                     selectedDays={props.selectedDate}
-                    initialMonth={new Date(2018, 7)}
-//                    disabledDays={ props.disabledDates.map((date) => new Date(date)) }
+//                    initialMonth={new Date(2018, 7)}
+                    month={new Date()}
+                    toMonth={new Date(2018, 11)}
+                    disabledDays={ props.disabledDates.map((date) => new Date(date)) }
 
-                disabledDays={[ props.disabledDates.map((date) => new Date(date)), 
-                              {
-//                               after: new Date(2017, 3, 20),
-                                before: new Date(),
-                            }, ]}
+
+//                disabledDays={[ props.disabledDates.map((date) => new Date(date)), 
+//                              {
+////                               after: new Date(2017, 3, 20),
+//                                before: new Date(),
+//                            }, ]}
 
                 />
                 <span className="chosenDate">Valt datum: {props.bdate}</span>
