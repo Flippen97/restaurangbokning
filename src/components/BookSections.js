fdd7fa6695fa4030d59e07607a19bb3d9baf8f79
Filip Ingemarsson <32750647+Flippen97@users.noreply.github.com>
2018-09-05 00:01:59 +0200

    Update BookSections.js

--- src/components/BookSections.js
+++ src/components/BookSections.js
@@ -26,43 +26,31 @@ function BookSections(props) {
         <React.Fragment>
         
             
-            {props.state.bookingStep === "1" ? (
+            {(props.state.bookingStep === "1") ? (
                 <div className="bookSection">
-                <h3>Välj ett datum:</h3>
-                <div className="divideSpace">
-                <DayPicker 
-                    onDayClick={props.onDayClick}
-                    selectedDays={props.state.selectedDate}
-                    month={new Date()}
-                    toMonth={new Date(2018, 11)}
-                    disabledDays={ props.state.disabledDates.map((date) => new Date(date))}
-                />
-                <span className="chosenDate">Valt datum: <br />{formateDate}</span>
-                </div>
-                <button className="nextButton" onClick={props.changeBokingStep} disabled={props.state.bdate === ''} value={"2"}>Nästa</button>
-                </div>) 
-                : (<React.Fragment />)}
-            
-            {props.state.bookingStep === "2" ? (
-                <div className="bookSection">
-                    <h3>Välj en sittning:</h3>
-                    <form>
-                        {props.state.availableAt18 ? <label className="radio inline"><input type="radio" onClick={props.setTime} data-btime="18" name="time" /><span>18:00</span></label>: <React.Fragment />}
-                        {props.state.availableAt21 ? <label className="radio inline"><input type="radio" onClick={props.setTime} data-btime="21" name="time" /><span>21:00</span></label>: <React.Fragment />}
-                    </form>
-                    Antal personer: <br />
-                    <Select
-                        className={"selectMenu"}
-                        placeholder="Antal personer"
-                        value={selectedOption}
-                        onChange={props.onChangeSelect}
-                        options={options}
-                    />
-
-                    <button className="nextButton" onClick={props.changeBokingStep} disabled={props.state.btime === '' || props.state.numberOfGuests === ''} value={"3"}>Nästa</button>
                 
-                </div>) 
-            : (<React.Fragment />)}
+                {props.state.fetchCalendarError ? <div className="fetchError">
+                    Någonting gick fel.<br />
+                    Vänligen uppdatera sidan och prova igen.
+                </div> : '' }
+             
+                {!props.state.fetchCalendarError ? <div> <h3>Välj ett datum:</h3>
+                        <div className="divideSpace">
+                        <DayPicker 
+                            onDayClick={props.onDayClick}
+                            selectedDays={props.state.selectedDate}
+                            month={new Date(2018, 8)}
+                            fromMonth={new Date(2018, 8)}
+                            toMonth={new Date(2018, 11)}
+                            disabledDays={ props.state.disabledDates.map((date) => new Date(date)) }
+                         />
+                        <span className="chosenDate">Valt datum: <br />{formateDate}</span>
+                        </div>
+                        <button className="nextButton" onClick={props.changeBokingStep} disabled={props.state.bdate === '' || props.state.fetchCalendarError} value={"2"}>Nästa</button>
+                            </div>
+                         : (<React.Fragment />)}
+                           
+                </div>) : '' }
             
 
             {props.state.bookingStep === "3" ? (
@@ -90,11 +78,15 @@ function BookSections(props) {
 
             {props.state.bookingStep === "4" ? (
                 <div className="bookSection">
+                  { !props.state.fetchBookingError 
+                     ? <div>
                     {props.state.name ? ` ${props.state.name}` : ""}, tack för din bokning hos oss!<br /><br />
                     Du är varmt välkommen den {formateDate} klockan {props.state.btime}.00, {props.state.numberOfGuests} personer<br /><br />
                     För avbokning vänligen kontakta oss via telefon eller email.<br />
                     070-000 00 00, FoodFusion@FoodFusion.com.
-
+                      </div> 
+                        : <div className="fetchError">Oj! Något gick fel, vänligen prova igen eller kontakta oss per telefon.</div>
+                    }
                     <button className="nextButton" 
                             onClick={props.changeBokingStep} 
                             value={"1"}>
